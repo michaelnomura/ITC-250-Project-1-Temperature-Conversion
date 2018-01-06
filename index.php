6814b4dc7cadd5c75cf92035f62afffeca47edff
Michael Nomura <mnnomura@gmail.com>
2018-01-06 14:29:26 -0800

    (Michael) Adds radio button and fixes some errors

--- index.php
+++ index.php
@@ -1,21 +1,50 @@
 <?php
 if(isset($_POST["Temperature"]))
 {//if there is data show it
-    echo $Temperature = $_POST["Temperature"];
+    echo $_POST["Temperature"];
+    echo '<br />';
+    echo $_POST["Type"];
 }else{//show form
     echo '
-    <form>
+    <form action="" method="post">
     <lable>
-    Temperature:
+        Temperature:
     <br />
     <input
-    type="number"
-    placeholder="Put temperature here"
-    required="required"
-    tabindex="10"
-    title="Temperature is required"    
-    </form>
+        type="number"
+        name="Temperature"
+        placeholder="Put temperature here"
+        required="required"
+        tabindex="10"
+        title="Temperature is required" />
+        </lable>
     <br />
+    <lable>
+        What type of temperature:
+        </lable>
+        <br />
+        
+    <input 
+        type="radio"
+        name="Type"
+        value="celcius" />
+        <lable>Celcius</lable>
+        <br />
+
+    <input 
+        type="radio" 
+        name="Type"
+        value="fahrenheit" />
+        <lable>Fahrenheit</lable>
+        <br />
+
+    <input 
+        type="radio" 
+        name="Type"
+        value="kelvin" />
+        <lable>Kelvin</lable>
+        <br />
+    
     <input type="submit" />
     </form>
     ';
