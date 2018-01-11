9c8eec8d5b312fc63ee2a1eb4cb7974c793a77b4
Aliya <aasken01@seattlecentral.edu>
2018-01-11 12:42:11 -0800

    Update index.php
    Altered some variable names, added switch block, and made converted output display above the form on the same page, so the user can continue to enter temperatures to convert without refreshing the page.  Also added function to ensure the user enters or selects all of the required fields.  Failing to do so will prompt the user to try again.

--- index.php
+++ index.php
@@ -1,166 +1,119 @@
 <?php
-
+//index.php
 require_once "Temperature.php";
 
 
-if(isset($_POST["Temperature"]))
-{//if there is data show it
-    //test output
-    echo $_POST["Temperature"];
-	$fromTemperature = $_POST["Temperature"];
-    echo '<br />';
-
-    echo $_POST["Type"];
-    $fromType = $_POST["Type"];
-    echo '<br />';
-    echo '<br />';
-
-////////////////////////////////////////////////////////////////////////////////////	
-//
-//  NOTE TO TEAM MEMBERS:
-//
-//  The following block of code is intended only as an example of
-//  how to create and initialize the Temperature object, as well as
-//  how to get the Kelvin, Celsius, and Fahrenheit temperatures out of it.
-//  
-//
-//  Create a new Temperature object and initialize it with the following arguments:
-//  $fromType can be "fahrenheit", "celsius", or "kelvin";
-//  $fromTemperature is any floating-point number or integer.
-//  In this case, both $fromType and $fromTemperature come from the $_POST variable.
-	$temperature = new Temperature($fromType, $fromTemperature);
-
-	echo '<pre>';  //  Preserve whitespace in the HTML that follows (optional).
-
-//  printf() means "PRINT with Formatting"  Bill's program specification requires
-//  rounding off our results to two decimal places.  The %8.2f part of the
-//  printf() function calls accomplish just that.
-	printf("%8.2f Kelvin<br />", $temperature->getKelvin());
-	printf("%8.2f °Celsius<br />", $temperature->getCelsius());
-	printf("%8.2f °Fahrenheit<br />", $temperature->getFahrenheit());
-
-	echo '</pre>';  //  Stop preserving whitespace.
-//
-//
-//  Aliya will replace this section with a more user-friendly output format.
-//	
-////////////////////////////////////////////////////////////////////////////////////	
-
+if(isset($_POST["Temperature"])){ //if there is data show it
 	
-/*    switch ($fromType)
-    {//determines the type of temperature being calculated
-        case "celsius":
-            switch ($toType)
-            {
-                case "celsius":
-                    //add equation;
-                    break;
-                case "fahrenheit":
-                    //add equation;
-                    break;
-                case "kelvin":
-                    //add equation;
-                    break;
-            }
-        case "fahrenheit":
-            switch ($toType)
-            {
-                case "celsius":
-                    //add equation;
-                    break;
-                case "fahrenheit":
-                    //add equation;
-                    break;
-                case "kelvin":
-                    //add equation;
-                    break;
-            }
-        case "kelvin":
-            switch ($toType)
-            {
-                case "celsius":
-                    //add equation;
-                    break;
-                case "fahrenheit":
-                    //add equation;
-                    break;
-                case "kelvin":
-                    //add equation;
-                    break;
-            }
-    }
-*/
-}else{//show form
-    echo '
-    <form action="" method="post">
-
-    <label>
-        Temperature:
-		<br />
-		<input
-			type="number"
-			name="Temperature"
-			placeholder="Put temperature here"
-			required="required"
-			tabindex="10"
-			title="Temperature is required" />
-	</label>
-    <br />
-
-    <label>
-        From:
-	</label>
-	<br />
+  	if(!isset($_POST["toType"]) || !isset($_POST["fromType"])){
+    	echo '<b>*Please enter a temperature and select desired conversion types below*</b><br />';
+        echo '<br />';
         
+    }else{
+        $fromTemperature = $_POST["Temperature"];
+        $toType = $_POST["toType"];
+        $fromType = $_POST["fromType"];
+
+        //echo '<br />';
+
+        $temperature = new Temperature($fromType, $fromTemperature);
+
+        echo '<pre>';  //  Preserve whitespace in the HTML that follows (optional).
+
+        echo $fromTemperature . ' ' . ($fromType == 'Kelvin' ? '' : 'degrees ') . $fromType . ' = ';
+
+        switch($toType){
+            case "Kelvin":
+                printf("%.2f Kelvin<br />", $temperature->getKelvin());
+                break;
+            case "Celsius":
+                printf("%.2f °Celsius<br />", $temperature->getCelsius());
+                break;
+            case "Fahrenheit":
+                printf("%.2f °Fahrenheit<br />", $temperature->getFahrenheit());
+                break;      		
+        }
+
+        echo '</pre>';  //  Stop preserving whitespace.
+    }
+}
+
+
+  echo '
+    <form action="" method="post">
+    <label>
+    Temperature: <br />
+    <input
+        type="number" 
+        name="Temperature" 
+        placeholder="Put temperature here" 
+        required="required" 
+        size="30"
+        maxlength="60" 
+        autofocus
+        title="Temperature is required" 
+        tabindex="10" 
+        />
+    </label>
+    
+    <br />
+    
+    <label>From:</label>
+    
+	<br /> 
+    
     <input 
-        type="radio"
-        name="Type"
-        value="celsius" />
+        type="radio" 
+        name="fromType" 
+        value="Celsius" />
 	<label>Celsius</label>
+    
 	<br />
 
     <input 
         type="radio" 
-        name="Type"
-        value="fahrenheit" />
+        name="fromType" 
+        value="Fahrenheit" />
 	<label>Fahrenheit</label>
+    
 	<br />
 
     <input 
         type="radio" 
-        name="Type"
-        value="kelvin" />
+        name="fromType" 
+        value="Kelvin" />
 	<label>Kelvin</label>
+    
 	<br />
 
     <label>
-		To:
+    To:
 	</label>
 	<br />
 
     <label>    
-		<input 
-			type="radio"
-			name="fromType"
-			value="celsius" />
-        <label>Celsius</label>
-        <br />
+    <input 
+        type="radio" 
+        name="toType" 
+        value="Celsius" />
+    <label>Celsius</label>
+    <br />
 
     <input 
         type="radio" 
-        name="fromType"
-        value="fahrenheit" />
+        name="toType" 
+        value="Fahrenheit" />
 	<label>Fahrenheit</label>
 	<br />
 
     <input 
         type="radio" 
-        name="fromType"
-        value="kelvin" />
+        name="toType" 
+        value="Kelvin" />
 	<label>Kelvin</label>
 	<br />
     
     <input type="submit" />
     </form>
-    ';
-}
+      ';
+
